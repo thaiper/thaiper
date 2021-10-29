732a8410070f6552a4bd1acd978fa0b80176c05e
gbezyuk <dev@gbezyuk.ru>
2021-10-29 14:37:00 +0300

    resolves #9 create app framework

---
+++ app/src/components/layout/PrimaryNavigation.vue
@@ -0,0 +1,5 @@
+<template>
+  <nav>
+    <router-link to="/">Thaiper</router-link>
+  </nav>
+</template>
---
+++ app/src/pages/index.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="page page-index">Thaiper Index</div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+
+export default defineComponent({
+})
+</script>
+
+<style lang="stylus">
+.home-page
+  //
+</style>
